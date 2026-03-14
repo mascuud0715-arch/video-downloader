e4e78bd5f234acea628997dd544d45eeb2f2f46c
mascuud0715-arch <mascuud0715@gmail.com>
2026-03-14 14:35:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, send_from_directory
 import yt_dlp
 import os
+import uuid
 
 app = Flask(__name__)
 
@@ -17,12 +18,15 @@ def home():
 
         url = request.form.get("url")
 
+        video_id = str(uuid.uuid4())[:8]
+
         ydl_opts = {
-            "outtmpl": f"{DOWNLOAD_FOLDER}/video.%(ext)s",
+            "outtmpl": f"{DOWNLOAD_FOLDER}/{video_id}.%(ext)s",
             "format": "best"
         }
 
         try:
+
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                 info = ydl.extract_info(url, download=True)
                 filename = ydl.prepare_filename(info)
@@ -39,6 +43,7 @@ def home():
 
 @app.route("/downloads/<path:filename>")
 def download_file(filename):
+
     return send_from_directory(DOWNLOAD_FOLDER, filename)
 
 
